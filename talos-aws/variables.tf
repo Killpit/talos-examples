fd38500c5e27caef5d73233a5525c5cd88c7fff0
Ata Tekeli <tekeliata2000@gmail.com>
2025-02-01 19:08:23 +0300

    dev: understood the project structure further

--- talos-aws/variables.tf
+++ talos-aws/variables.tf
@@ -1,86 +1,97 @@
 variable "name" {
   description = "Project name, required to create unique resource names"
-  type = "string"
-  default = "talos-aws-example"
+  type        = string
+  default     = "talos-aws-example"
 }
 
 variable "cluster_architecture" {
-  default = "amd64"
   description = "Cluster architecture. Choose 'arm64' or 'amd64'. If you choose 'arm64', ensure to also override the control_plane.instance_type"
-  type = string
+  type        = string
+  default     = "amd64"
 }
 
 variable "control_plane" {
-  default = {}
   description = "Info for control plane that will be created"
   type = object({
-    instance_type = optional(string, "m5.large")
-    config_patch_files = optional(list(string), [])
-    tags = optional(map(string), {})
+    instance_type        = optional(string, "m5.large")
+    config_patch_files   = optional(list(string), [])
+    tags                 = optional(map(string), {})
   })
+  default = {}
 }
 
 variable "worker_groups" {
-  default = [{
-    name = "default"
-  }]
-  description = "List of node worker node groups to create"
+  description = "List of worker node groups to create"
   type = list(object({
-    name = string
-    instance_type = optional(string, "m5.large")
-    config_patch_files = optional(list(string), [])
-    tags = optional(map(string), {})
+    name                = string
+    instance_type       = optional(string, "m5.large")
+    config_patch_files  = optional(list(string), [])
+    tags                = optional(map(string), {})
   }))
+  default = []
 }
 
 variable "region" {
-    description = "The region name"
-    type = "string"
+  description = "The AWS region where the cluster will be deployed"
+  type        = string
 }
 
 variable "owner" {
   description = "Owner for resource tagging"
-  type = string
+  type        = string
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-  description = "The CIDR to use for the VPC. Currently it must be a /16 or /24"
-  type = string
+  description = "The CIDR to use for the VPC. Must be a /16 or /24."
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "tags" {
-  default = {
-    usage = "aws-example",
-    platform = "talos"
-  }
-  description = "The set of tags to place on the created resources. These will be merged with the default tags defined via local.tags in 00-locals.tf"
-  type = map(string)
+variable "service_cidr" {
+  description = "The CIDR range for Kubernetes Services"
+  type        = string
+  default     = "100.68.0.0/16"
 }
 
-# talos module
+variable "pod_cidr" {
+  description = "The CIDR range for Kubernetes Pods"
+  type        = string
+  default     = "100.64.0.0/14"
+}
+
+variable "allocate_node_cidrs" {
+  description = "Assign PodCIDRs to Node resources (Only needed if using Cilium Kubernetes IPAM mode)"
+  type        = bool
+  default     = false
+}
+
+# Kubernetes & Talos Configuration
 variable "talos_version" {
-  default = "v1.9.2"
-  description = "Talos version to use for the cluster, if not set the newest Talos version. Check https://github.com/siderolabs/talos/releases for available releases"
-  type = string
+  description = "Talos version to use for the cluster. Check https://github.com/siderolabs/talos/releases"
+  type        = string
+  default     = "v1.9.2"
 }
 
 variable "kubernetes_version" {
-  default = "v1.32.0"
-  description = "Kubernetes version to use for the Talos cluster, if not set, the K8s version shipped with the selected Talos version will be used. Check https://www.talos.dev/latest/introduction/support"
+  description = "Kubernetes version to use for the Talos cluster. Check https://www.talos.dev/latest/introduction/support"
+  type        = string
+  default     = "v1.32.0"
 }
 
-variable "network_shift" {
-    description = "Network number shift"
-    type = number
-    default = 2
+variable "disable_kube_proxy" {
+  description = "Determines whether Kube-Proxy should be deployed (set to true if using an alternative CNI like Cilium)"
+  type        = bool
+  default     = false
 }
 
+# Consolidated Resource Tags
 variable "tags" {
-    description = "Tags of resources"
-    type = map(string)
-    default = {
-      Name = "talos"
-      Environment = "Develop"
-    }
+  description = "The set of tags to place on the created resources."
+  type        = map(string)
+  default = {
+    usage       = "aws-example"
+    platform    = "talos"
+    Name        = "talos"
+    Environment = "Develop"
+  }
 }
